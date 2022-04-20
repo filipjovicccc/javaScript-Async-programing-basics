0913f2b7b88df66b3a5f9217cf2c2ff474f8b171
filipjovicccc <filipjovicccc@gmail.com>
2022-04-20 21:49:18 +0200

    update

---
+++ promise/promise.js
@@ -0,0 +1,31 @@
+const postExamples = [
+  { animal: "cat", color: "orange", age: 9 },
+  { animal: "dog", color: "brown", age: 2 },
+];
+
+function getPostExample() {
+  setTimeout(() => {
+    display = "";
+    postExamples.forEach((post, index) => {
+      display += `<li>${post.animal} ${post.age} ${post.color}</li>`;
+    });
+
+    document.body.innerHTML = display;
+  }, 1000);
+}
+function createNewPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      postExamples.push(post);
+      const error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject("error something went wrong");
+      }
+    }, 2000);
+  });
+}
+createNewPost({ animal: "tiger", color: "yellow", age: 5 }).then(
+  getPostExample
+);
